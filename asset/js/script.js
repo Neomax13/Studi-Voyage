c267a460bdc57b22b7897ff77348844e2cb3a154
Guillaue Biagini <guillaumebiagini@gmai.com>
2024-08-21 17:50:04 +0200

    c,dzkvnd

---
+++ asset/js/script.js
@@ -0,0 +1,48 @@
+/**
+ * Array Methods
+ * toString()
+ * join()
+ * pop()
+ * push()
+ * shift()
+ * unshift()
+ */
+
+//tostring
+//array.tostring();
+/**let languages = ["JS","JAVA","PHP","C","C++"];
+console.log(languages.tostring());*/
+
+//join
+//array.join(separator);
+//let languages = ["JS","JAVA","PHP","C","C++"];
+//console.log(languages.join());
+//console.log(languages.join(""));
+//console.log(languages.join("-"));
+
+//pop
+//array.pop();
+//let languages = ["JS","JAVA","PHP","C","C++"];
+//console.log(languages.pop());
+
+//push
+//array.push();
+//let languages = ["JS","JAVA","PHP","C","C++"];
+//console.log(languages.push("CSS","RUBY"));
+//console.log(languages);
+
+//shift
+//array.shift();
+//let languages = ["JS","JAVA","PHP","C","C++"];
+//console.log(languages.shift());
+//console.log(languages);
+
+
+//unshift
+//array.unshift();
+let languages = ["JS","JAVA","PHP","C","C++"];
+console.log(languages.unshift());
+console.log(languages);
+
+
+
